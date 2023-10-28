a92fdc8840ccb899d155d8ad2cdc6377e30b7358
Yair5675 <yairziv5675@gmail.com>
2023-10-28 21:54:07 +0300

    Changed the Addition class to represent the sum of an unlimited amount of functions, not just two

--- functions/operations/Addition.java
+++ functions/operations/Addition.java
@@ -1,15 +1,17 @@
 package functions.operations;
 
 import functions.Function;
+import java.util.Arrays;
 
 
 /**
- * A class that represents addition of any two functions.
+ * A class that represents addition of multiple functions.
  */
-public record Addition(Function fx, Function gx) implements Function {
+public record Addition(Function[] functions) implements Function {
 
     @Override
     public double compute(double x) {
-        return fx.compute(x) + gx.compute(x);
+        // Sum the results of calling 'compute' on every function:
+        return Arrays.stream(this.functions).mapToDouble((function -> function.compute(x))).sum();
     }
 }
