40af23db8b009d01d577c00fc05f7c5f0fab2467
Yair5675 <yairziv5675@gmail.com>
2023-10-28 21:59:23 +0300

    Changed the Multiplication class to represent the product of an unlimited amount of functions, not just two

--- functions/operations/Multiplication.java
+++ functions/operations/Multiplication.java
@@ -1,14 +1,18 @@
 package functions.operations;
 
 import functions.Function;
+import java.util.Arrays;
 
 /**
- * A class that represents multiplication between two functions.
+ * A class that represents the product between multiple functions.
  */
-public record Multiplication(Function fx, Function gx) implements Function {
+public record Multiplication(Function[] functions) implements Function {
 
     @Override
     public double compute(double x) {
-        return fx.compute(x) * gx.compute(x);
+        // Multiply all the results of the functions together:
+        return Arrays.stream(this.functions)
+                .mapToDouble(function -> function.compute(x))
+                .reduce(1.0, (accumulator, element) -> accumulator * element);
     }
 }
