1ed36e9ec6c9286c8221568b5e3ec69783871f66
Yair5675 <yairziv5675@gmail.com>
2023-10-28 18:08:52 +0300

    Created a Polynomial Term class to represent every expression that is raised to a power and multiplied by a scalar. The expression is not necessarily one variable

---
+++ PolyTerm.java
@@ -0,0 +1,10 @@
+public class PolyTerm {
+    // The coefficient of the polynomial term:
+    private double scalar;
+
+    // The body of the polynomial term (the variable):
+    private Function body;
+
+    // The power that the body is raised to:
+    private double power;
+}
