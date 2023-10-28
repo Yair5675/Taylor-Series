af1cfb2e69a5934d0637c374be69892e3ad20e0d
Yair5675 <yairziv5675@gmail.com>
2023-10-28 22:08:23 +0300

    Made the body of PolyTerm to implement both the Function AND Differentiable interfaces, and made PolyTerm implement Differentiable

--- functions/PolyTerm.java
+++ functions/PolyTerm.java
@@ -10,10 +10,17 @@ package functions;
  * @param body
  * @param power
  */
-public record PolyTerm(double scalar, Function body, double power) implements Function {
+public record PolyTerm<T extends Function & Differentiable>(double scalar, T body, double power)
+        implements Function, Differentiable {
 
     @Override
     public double compute(double x) {
         return this.scalar * Math.pow(this.body.compute(x), this.power);
+    }
+
+    @Override
+    public <F extends Function & Differentiable> F differentiate() {
+        // TODO
+        return null;
     }
 }
