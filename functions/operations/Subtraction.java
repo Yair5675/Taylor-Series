4bec614482abba042d1e37ed6decd098b9adadba
Yair5675 <yairziv5675@gmail.com>
2023-10-28 18:30:19 +0300

    Created a record to represent subtraction of two functions. The record itself is a function too

---
+++ functions/operations/Subtraction.java
@@ -0,0 +1,14 @@
+package functions.operations;
+
+import functions.Function;
+
+/**
+ * A class that represents subtraction of any two functions.
+ */
+public record Subtraction(Function fx, Function gx) implements Function {
+
+    @Override
+    public double compute(double x) {
+        return fx.compute(x) - gx.compute(x);
+    }
+}
