5fef563e95c6a25976d7e12d321d16e52271d687
Yair5675 <yairziv5675@gmail.com>
2023-10-28 19:02:51 +0300

    Added in inner class called TermNode to represent a single node in the function chain

--- functions/TermList.java
+++ functions/TermList.java
@@ -1,4 +1,23 @@
 package functions;
 
 public class TermList {
+    /**
+     * A class to represent a function within another function and to serve as a node in the overall function chain.
+     */
+    private static class TermNode {
+        // The previous function (in which the current function is):
+        private TermNode prev;
+
+        // The function of the current node:
+        private Function func;
+
+        // The next function (which is the function within the current function:
+        private TermNode next;
+
+        public TermNode(TermNode prev, Function func, TermNode next) {
+            this.prev = prev;
+            this.func = func;
+            this.next = next;
+        }
+    }
 }
