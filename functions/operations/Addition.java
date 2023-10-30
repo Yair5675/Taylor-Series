5374b6b15912e4e7faf9d9aaa02f0c7855d371fe
Yair5675 <yairziv5675@gmail.com>
2023-10-30 15:31:44 +0200

    Converted the Addition record from a record to a class to allow for customizing getters and constructor in the future. Also changed the data type of the functions in the addition from an array to ArrayList for better utility functions

--- functions/operations/Addition.java
+++ functions/operations/Addition.java
@@ -1,18 +1,30 @@
 package functions.operations;
 
 import functions.Function;
-import java.util.Arrays;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
  * A class that represents addition of multiple functions.
  */
-public record Addition(Function[] functions) implements Function {
+public class Addition implements Function {
+    // The functions which are summed up:
+    private final ArrayList<Function> functions;
+
+    public Addition(Function ... functions) {
+        this.functions = (ArrayList<Function>) List.of(functions);
+    }
+
+    public ArrayList<Function> getFunctions() {
+        return functions;
+    }
 
     @Override
     public double compute(double x) {
         // Sum the results of calling 'compute' on every function:
-        return Arrays.stream(this.functions).mapToDouble((function -> function.compute(x))).sum();
+        return this.functions.stream().mapToDouble((function -> function.compute(x))).sum();
     }
 
     @Override
