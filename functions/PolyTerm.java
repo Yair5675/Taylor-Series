7821ec7af134a20cc38f613a213503fef3e7eef7
Yair5675 <yairziv5675@gmail.com>
2023-10-30 15:11:32 +0200

    Improved the toString of polynomial terms (removed unnecessary zeros, scalars and powers if equal to 1, etc...)

--- functions/PolyTerm.java
+++ functions/PolyTerm.java
@@ -26,6 +26,35 @@ public record PolyTerm(double scalar, double power) implements Function {
 
     @Override
     public String toString() {
-        return String.format("%fx^(%f)", this.scalar, this.power);
+        // If the scalar is zero, return zero:
+        if (this.scalar == 0) return "0";
+
+        // Removing the tailing zeroes from the scalar and the power:
+        String scalar = Double.toString(this.scalar).replaceAll("0*?", "");
+        String power = Double.toString(this.power).replaceAll("0*?", "");
+
+        // If the scalar or power are actually integers:
+        scalar = this.scalar == (int) this.scalar ? Integer.toString((int) this.scalar) : scalar;
+        power = this.power == (int) this.power ? Integer.toString((int) this.power) : power;
+
+        // If the power is 0, this is a constant:
+        if (this.power == 0)
+            return scalar;
+
+        // If the scalar or power is equal to one:
+        if (this.scalar == 1)
+            scalar = "";
+        if (this.power == 1)
+            power = "";
+
+        // Constructing the final toString message:
+        if (scalar.isEmpty() && power.isEmpty())
+            return "x";
+        else if (scalar.isEmpty())
+            return String.format("x^%s", power);
+        else if (power.isEmpty())
+            return String.format("%sx", scalar);
+        else
+            return String.format("%sx^%s", scalar, power);
     }
 }
