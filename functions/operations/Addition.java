a1c4836a3eca10070a2cefb9ec445589a546547c
Yair5675 <yairziv5675@gmail.com>
2023-10-31 14:03:11 +0200

    Implemented a custom 'toString' method for the Addition class

--- functions/operations/Addition.java
+++ functions/operations/Addition.java
@@ -1,7 +1,6 @@
 package functions.operations;
 
 import functions.Function;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,5 +26,28 @@ public class Addition implements Function {
     public Function differentiate() {
         // The derivative of a sum is the sum of derivatives:
         return new Addition(this.functions.stream().map(Function::differentiate).toArray(Function[]::new));
+    }
+
+    @Override
+    public String toString() {
+        if (this.functions.isEmpty()) return "";
+
+        final StringBuilder sb = new StringBuilder();
+
+        String nextFunc = this.functions.get(0).toString();
+        for (int i = 0; i < this.functions.size(); i++) {
+            // Enter the current function's toString:
+            sb.append(nextFunc);
+
+            // Load the toString of the next func:
+            if (i < this.functions.size() - 1) {
+                nextFunc = this.functions.get(i + 1).toString();
+                // Add a '+' only if the next function is not negative:
+                if (!nextFunc.startsWith("-"))
+                    sb.append(" + ");
+            }
+        }
+
+        return sb.toString();
     }
 }
