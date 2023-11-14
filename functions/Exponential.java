4503b24ee90d8ce0da9c6a6dc24eddc9de572b90
Yair5675 <yairziv5675@gmail.com>
2023-11-14 09:21:19 +0200

    Added custom base to Exponential class

--- functions/Exponential.java
+++ functions/Exponential.java
@@ -1,20 +1,48 @@
 package functions;
 
 import functions.interfaces.Function;
+import functions.operations.Multiplication;
 
 public class Exponential implements Function {
+    private double base;
+
+    public Exponential() {
+        this.base = Math.E;
+    }
+
+    public Exponential(double base) {
+        this.base = base;
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
     @Override
     public double compute(double x) {
-        return Math.exp(x);
+        return Math.pow(this.base, x);
     }
 
     @Override
     public Function differentiate() {
-        return this;
+        // Derivative of e^x is e^x:
+        if (this.base == Math.E)
+            return this;
+
+        // Derivative of a^x is a^x * ln(a):
+        return new Multiplication(this, new PolyTerm(Math.log(this.base), 0));
     }
 
     @Override
     public String toString() {
-        return "e^x";
+        // Get the base:
+        String base = Double.toString(this.base).replaceAll("0*?", "");
+        base = this.base == (int) this.base ? Integer.toString((int) this.base) : base;
+
+        return String.format("%s^x", base);
     }
 }
